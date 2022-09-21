f1bbd533ae3be8f35a6933a20cd0859592537e2e
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-09-21 15:27:28 +0000

    set up, with file

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int main() {
   
   // LOAD IN FILE
+  ifstream inFile;    //declare stream variabl
+  inFile.open("NumberData1.txt");
 
   // LOAD IN ALL DATA IN WHILE LOOP
-  
+  while (!inFile.eof())
+    {
+      
+    }
   
 }
